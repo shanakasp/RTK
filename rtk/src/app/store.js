5b82d521c58715b960cea1db68c046c836a06856
shanakasp <74164029+shanakasp@users.noreply.github.com>
2024-03-18 16:51:13 +0530

    changed 5 files

--- rtk/src/app/store.js
+++ rtk/src/app/store.js
@@ -1,8 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
-import counterReducer from "../features/counter/CounterSlice";
 
 export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
+  reducer: {},
 });
